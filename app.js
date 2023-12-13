2d7a3629b3570f553f6a0b7f36517b5bf6fb163b
midu-zack <midlajmidu0002@gmail.com>
2023-12-13 20:56:42 +0530

    submitbutton

--- app.js
+++ app.js
@@ -1,30 +1,80 @@
 const express = require("express");
 const app = express();
-
-// Serve static files from the "crudapp" folder
-
-app.use(express.static("pages"));
-
-const name = "Muhammed midlaj"
-const age = 18
-
-
-app.post('/submit',(req,res)=>{
-  console.log(req.body);
-  
-})
+const path = require('path');
+const ejs = require('ejs');
+const bodyparser = require('body-parser');
+const fs = require('fs');
+const { send } = require("process");
+const { get } = require("http");
 
 
+app.set("views", "./views")
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(bodyparser.json());
 
-
-// Define your routes or other middleware here
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-
-  console.log(`Server is running on port ${PORT}`);
-
+// home
+app.get("/"|| "/home?", (req, res) => {
+  const data = {
+    formpage: "miduzack",
+    user: null,
+  };
+  res.render("index", data);
 });
 
+// table
+app.get('/home?', (req, res) => {
+  res.render("table");
+});
+
+// index
+// app.get('/form?',(req,res)=>{
+//   res.render("index")
+// })
+
+
+// Submit
+app.post("/submit",(req,res)=>{
+  const userdata = req.body;
+  // console.log(userdata);
+
+  fs.readFile("./data/data.json","utf-8",(err,data)=>{
+    if(err){
+      console.error("Error while reading users.json");
+      res.status(500).send("Error while reading users.json");
+    }else{
+      try{
+        const existingdata = JSON.parse(data)
+        userdata.id =existingdata.length+1;
+        existingdata.push(userdata)
+
+        fs.writeFile("./data/data.json",JSON.stringify(existingdata,null,2),(Errorcode)=>{
+          if(writeError){
+            console.error("Error, adding data bise",Errorcode)
+
+          }else{
+            res.status(500)
+            res.send("new user added ")
+          }
+
+        })
+      }catch(error){
+        console.error("unexpected error");
+        res.status(500).send(`Unexpected error:${error}`);
+
+      }
+    }
+  }
+  );
+});
+
+ 
+ 
+
+ 
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
